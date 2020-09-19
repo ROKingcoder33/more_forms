3edffec6963d53aee94149d983003e7cbba4790a
Ralph King Jr <ralph@kingralph.dev>
2020-09-19 17:25:41 -0400

    updated form appearance

--- src/components/forms/registration.js
+++ src/components/forms/registration.js
@@ -6,7 +6,7 @@ const Registration = props => {
     return (
         <>
             <Container>
-                <Row className="justify-content-center">
+                <Row className="justify-content-center pt-5">
                     <Form>
                         <Form.Group as={Row} controlId="formGroupFirstName">
                             <Form.Label column="lg" lg={4}>
@@ -15,14 +15,53 @@ const Registration = props => {
                             <Col lg={8}>
                                 <Form.Control type="text" size="lg" placeholder="Type your first name" />
                                 <Form.Text className="text-muted">
-                                    ***This will be a validation***
+                                    First Name must be at least 2 characters 
                                 </Form.Text>
                             </Col>
                         </Form.Group>
-                        <Form.Group as={Row}>
-                            <Button variant="outline-primary" type="submit">
-                                Submit
-                            </Button>
+                        <Form.Group as={Row} controlId="formGroupLastName">
+                            <Form.Label column="lg" lg={4}>
+                                Last Name
+                            </Form.Label>
+                            <Col lg={8}>
+                                <Form.Control type="text" size="lg" placeholder="Type your last name" />
+                                <Form.Text className="text-muted">
+                                    Last Name must be at least 2 characters 
+                                </Form.Text>
+                            </Col>
+                        </Form.Group>
+                        <Form.Group as={Row} controlId="formGroupEmail">
+                            <Form.Label column="lg" lg={4}>
+                                Email
+                            </Form.Label>
+                            <Col lg={8}>
+                                <Form.Control type="text" size="lg" placeholder="Type your Email" />
+                                <Form.Text className="text-muted">
+                                    Email must be at least 5 characters 
+                                </Form.Text>
+                            </Col>
+                        </Form.Group>
+                        <Form.Group as={Row} controlId="formGroupPassword">
+                            <Form.Label column="lg" lg={4}>
+                                Password
+                            </Form.Label>
+                            <Col lg={8}>
+                                <Form.Control type="text" size="lg" placeholder="Type your Password" />
+                                <Form.Text className="text-muted">
+                                    Password must be at least 8 characters 
+                                </Form.Text>
+                                <Form.Text className="text-muted">
+                                    Passwords must match 
+                                </Form.Text>
+                            </Col>
+                        </Form.Group>
+                        <Form.Group as={Row} controlId="formGroupConfirmPassword">
+                            <Form.Label column="lg" lg={4}>
+                                Confirm Password
+                            </Form.Label>
+                            <Col lg={8}>
+                                <Form.Control type="text" size="lg" placeholder="Type your Password" />
+                            </Col>
                         </Form.Group>
                     </Form>
                 </Row>
